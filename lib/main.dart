0a66bf1d3d783d4a3bc7285978097bb760d9a8f4
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2024-03-06 13:43:51 +0100

    chore: add dependencies, boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// Copyright 2024 Sifterstudios
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,28 +13,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    final materialLight = FlexThemeData.light(scheme: FlexScheme.mandyRed);
+    final materialDark = FlexThemeData.dark(scheme: FlexScheme.mandyRed);
+    final cupertinoLight =
+        MaterialBasedCupertinoThemeData(materialTheme: materialLight);
+    final cupertinoDark =
+        MaterialBasedCupertinoThemeData(materialTheme: materialDark);
+
+    return PlatformProvider(
+      builder: (context) => PlatformTheme(
+        themeMode: ThemeMode.system,
+        materialLightTheme: materialLight,
+        materialDarkTheme: materialDark,
+        cupertinoLightTheme: cupertinoLight,
+        cupertinoDarkTheme: cupertinoDark,
+        builder: (context) => const PlatformApp(
+          title: 'My Title',
+          localizationsDelegates: [DefaultMaterialLocalizations.delegate],
+          home: MyHomePage(
+            title: 'Second Title',
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -81,7 +84,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
