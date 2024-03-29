2d17926ab39fae75227dbe93115e4fda892a584d
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2024-03-29 21:04:14 +0100

    Absolutely 100%

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,10 +1,14 @@
+// Copyright 2024 Sifterstudios
+
 import 'package:blekker/app/app.dart';
 import 'package:blekker/features/auth/presentation/pages/signup_page.dart';
+import 'package:blekker/init_dependencies.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders SignupPage', (tester) async {
+      await serviceLocator.reset();
       await tester.pumpWidget(const App());
       expect(find.byType(SignupPage), findsOneWidget);
     });
